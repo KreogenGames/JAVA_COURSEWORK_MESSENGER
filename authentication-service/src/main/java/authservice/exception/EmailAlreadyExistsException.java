dc071fbf006f0089a5f14e341f0e9a60089ded8c
Марис <70753488+KreogenGames@users.noreply.github.com>
2021-10-08 18:38:57 +0300

    auth-service full

---
+++ authentication-service/src/main/java/authservice/exception/EmailAlreadyExistsException.java
@@ -0,0 +1,8 @@
+package authservice.exception;
+
+public class EmailAlreadyExistsException extends RuntimeException {
+
+    public EmailAlreadyExistsException(String message) {
+        super(message);
+    }
+}
