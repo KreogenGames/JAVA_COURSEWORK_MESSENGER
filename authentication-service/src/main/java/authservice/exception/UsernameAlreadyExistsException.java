1c886de45a74ca1605a6f5ca3d58c1e8732b109e
Марис <70753488+KreogenGames@users.noreply.github.com>
2021-11-01 12:25:43 +0300

    Delete authentication-service directory
    wrong auth-service realization

--- authentication-service/src/main/java/authservice/exception/UsernameAlreadyExistsException.java
+++
@@ -1,8 +0,0 @@
-package authservice.exception;
-
-public class UsernameAlreadyExistsException extends RuntimeException {
-
-    public UsernameAlreadyExistsException(String message) {
-        super(message);
-    }
-}
